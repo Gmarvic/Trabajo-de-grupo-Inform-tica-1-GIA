4778944257d6e0fac1d4832120fa8a05bbf159f8
Gmarvic <83537095+Gmarvic@users.noreply.github.com>
2021-05-03 00:30:50 +0200

    Update modules.f95

--- fortran-files/modules.f95
+++ fortran-files/modules.f95
@@ -47,8 +47,57 @@ subroutine jacob_f(x,a,m)
            a(1,2) = (-y2(1)+y1(1))/h 
            a(2,2) = (-y2(2)+y1(2))/h 
        end subroutine 
+   
+!subrutinas de LU:
+
+!factorización en lower y upper:
+subroutine factorizar (a,n) 
+       implicit none 
+       ! variables y argumentos 
+          integer :: k, i, h, j, n 
+          real*8 :: a(n,n), s 
+          !procedemos con la factorizacion 
+            do k = 1, n 
+               do i = k, n 
+                 s = 0 
+                 do h =1, k-1 
+                    s = s + a(i,h) * a(h,k) 
+                 end do 
+               a(i,k) = a(i,k) - s 
+               end do 
+               do j = k+1, n 
+                 s = 0 
+                 do h = 1, k-1 
+                   s = s + a(k,h) * a(h,j) 
+                 end do 
+                 a(k,j) = (1. / a(k,k)) * (a(k,j) - s) 
+               end do 
+            end do 
+       end subroutine
        
-       
+!resolución del sistema:
+subroutine sustituir (a,b,n) 
+            implicit none 
+            ! variables  
+            integer :: n, k, h 
+            real*8 :: a(n,n), b(n), s 
+            ! resolución:
+              do k = 1, n 
+                 s = 0 
+                 do h = 1, k-1 
+                 s = s + a(k,h) * b(h) 
+                 end do 
+                 b(k) = (b(k) - s) / a(k,k) 
+              end do 
+              do k = n, 1, -1 
+                 s= 0 
+                 do h = k+1, n 
+                    s = s + a(k,h) * b(h) 
+                 end do 
+              b(k) = b(k) - s 
+              end do 
+      end subroutine 
+
 !-------------------------------------------------------------------------------
 
 
@@ -88,10 +137,6 @@ subroutine jacob_f(x,a,m)
 
   end function
 
-
-
-
-
   function f(x,ro,mu)
 
     ! función para el cálculo de probabilidades
@@ -107,34 +152,6 @@ subroutine jacob_f(x,a,m)
     f= (1d0/(ro*sqrt(2d0*pi)))*exp((-(x-mu)**2d0)/(2d0*(ro**2d0)))
 
   end function
-
-
-
-  function norma(vector,n)
-
-    ! norma euclídea de un vector de dimensión n
-
-    integer, intent(in) :: n
-
-    real*8, intent (in) :: vector(n)
-
-    real*8 :: norma
-
-    integer :: i
-
-    norma=0d0
-
-    do i=1, n
-
-      norma=norma + vector(i)**2
-
-    end do
-
-    norma=sqrt(norma)
-
-  end function
-
-
 
   subroutine escribir_matriz(a,n,m)
 
@@ -155,107 +172,4 @@ subroutine jacob_f(x,a,m)
     end do
 
   end subroutine
-
-
-
-  ! modulos de LU:
-
-
-
-  subroutine factorizar (a,n)
-
-    ! a, matriz; n, dimensión
-
-    ! almacena en a la solución
-
-    implicit none
-
-    integer, intent(in) :: n
-
-    real*8, intent(inout) :: a(n,n)
-
-    integer :: k, i, h, j
-
-    real*8 :: s
-
-    do k = 1, n
-
-      do i = k, n
-
-        s = 0
-
-        do h =1, k-1
-
-          s = s + a(i,h) * a(h,k)
-
-        end do
-
-        a(i,k) = a(i,k) - s
-
-      end do
-
-      do j = k+1, n
-
-        s = 0
-
-        do h = 1, k-1
-
-          s = s + a(k,h) * a(h,j)
-
-        end do
-
-        a(k,j) = (1 / a(k,k)) * (a(k,j) - s)
-
-      end do
-
-    end do
-
-  end subroutine
-
-
-
-  subroutine sustituir (a,b,n)
-
-    implicit none
-
-    integer, intent(in) :: n
-
-    real*8, intent(inout) :: a(n,n), b(n)
-
-    integer :: k, h
-
-    real*8 :: s
-
-    do k = 1, n
-
-      s = 0
-
-      do h = 1, k-1
-
-        s = s + a(k,h) * b(h)
-
-      end do
-
-      b(k) = (b(k) - s) / a(k,k)
-
-    end do
-
-    do k = n, 1, -1
-
-      s= 0
-
-      do h = k+1, n
-
-        s = s + a(k,h) * b(h)
-
-      end do
-
-      b(k) = b(k) - s
-
-    end do
-
-  end subroutine
-
-
-
-End module
+  end module
