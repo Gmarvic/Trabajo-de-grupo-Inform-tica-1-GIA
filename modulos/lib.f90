13dcd6d97068734f1e1bbd35d71d074126d9f992
marcoaias <74058672+marcoaias@users.noreply.github.com>
2021-05-02 20:33:07 +0200

    upload

---
+++ modulos/lib.f90
@@ -0,0 +1,238 @@
+module lib
+  implicit none
+
+contains
+
+  ! Muestra matrices de reales (m,n) por pantalla
+  subroutine show_array(A, m, n)
+    implicit none
+    integer, intent(in) :: m, n
+    real(8), intent(in) :: A(m,n)
+
+    integer :: i
+
+    do i = 1, m
+      !print *, A(i,:)
+    end do
+
+  end subroutine
+
+  ! Convierte un entero en una cadena de characteres
+  character(len=20) function str(k)
+    integer, intent(in) :: k
+    write (str, *) k
+    str = adjustl(str)
+  end function str
+
+
+  ! funcion distribución gauss, x, media, desviación, f(x)
+  function gauss(x, mu, sigma) result(f)
+    implicit none
+    real(8), intent(in) :: x, mu, sigma
+    real(8) :: f
+
+    f = exp(-0.5 * ((x-mu)/sigma)**2)/(sigma*sqrt(2*acos(-1.0)))
+  end function
+
+
+  ! calcula la función de probabilidad acumulada, según t, para una distribución de gauss
+  function probabilidad(t, mu, sigma) result(P)
+    implicit none
+    real(8), intent(in) :: t, mu, sigma
+    real(8) :: P, s
+
+    call integralCDF(t, mu, sigma, 1000, s)
+    P = 0.5 + s
+  end function
+
+
+  ! Según el método de simpson se integra la probabilidad compuesta en función a mu y sigma
+  ! Función de probabilidad acumulada, CDF
+  ! límite de integración t, distribución(mu, sigma), número de intervalos n, solución s
+  subroutine integralCDF(t,mu,sigma,n,s)
+    implicit none
+    integer, intent(in) :: n
+    real(8), intent(in) :: t, mu, sigma
+    real(8), intent(inout) :: s
+
+    real(8) :: s1, s2, h, x
+    integer :: i
+
+    h = (t-mu)/dble(n)
+
+    s1 = 0d0
+    do i = 1, n-1,2
+      x = mu + h*dble(i)
+      s1 = s1 + gauss(x, mu, sigma)
+    end do
+
+    s2 = 0d0
+    do i = 2, n-2, 2
+      x = mu + h*dble(i)
+      s2 = s2 + gauss(x, mu, sigma)
+    end do
+
+    s = (h/dble(3))*(gauss(mu, mu, sigma) + gauss(t, mu, sigma) + dble(4)*s1 + dble(2)*s2)
+  end subroutine
+
+
+
+  ! Esta subrutina calcula los valores emsr relevantes, según el algoritmo EMSR-b, desde y hacia la tabla A(filas, 6), ordenada descendentemente en función de las tarifas de las clases.
+  ! La tabla A() se usará para calcular los niveles de protección para cada clase.
+
+  subroutine valores_emsr(A, filas)
+    implicit none
+    integer, intent(in) :: filas
+    real(8), intent(inout) :: A(filas, 6)
+
+    integer :: i, k
+    real(8) :: s
+
+    do i = 1, filas
+
+      s = 0.d0
+      do k = 1, i
+        s = s + A(k, 2)
+      end do
+
+      A(i,5) = s
+
+      s = 0.d0
+      do k = 1, i
+        s = s + A(k, 1)*A(k, 2)
+      end do
+
+      A(i,4) = s/A(i,5)
+
+      s = 0.d0
+      do k = 1, i
+        s = s + A(k, 3)*A(k, 3)
+      end do
+
+      A(i, 6) = sqrt(s)
+    end do
+  end subroutine
+
+
+  ! aplica la regla de Littlewood; compara cada clase al conjunto restante según el algoritmo EMSRb;
+  ! recibe como parámetros la tabla de los valores emsr calculados en la subrutina valores_emsr, el número de filas (clases) que contiene la tabla, y el vector solución v que contiene los niveles de protección para cada clase comparado con el resto.
+  ! véase:
+  ! https://youtu.be/mZY4CU05PLw
+  !TODO !print aid
+  subroutine proteger (A, filas, v)
+    implicit none
+    integer, intent(in) :: filas
+    real(8), intent(in) :: A(filas, 6)
+    real(8), intent(inout) :: v(filas)
+
+    integer :: i, j
+    real(8) :: s, x, tol, dx
+
+    v = 0.d0
+    ! se define la tolerancia
+    tol = dble(0.0000000001)
+    do i = 1, filas-1
+
+      ! solución inicial centrada en mu siempre garantiza convergencia
+      x = A(i,5)
+
+      ! resolución iterativa según la linealización del complementario cdf, quasi método de Newton
+
+      do j = 1, 50
+        s = A(i+1, 1)/A(i, 4) - 1 + probabilidad(x, A(i,5), A(i,6))
+
+        if (abs(s) < tol) then
+          v(i) = x
+          !print *, i, v(i), j
+
+          !print *, A(i+1, 1)/A(i, 4)
+          !print *, 1 - probabilidad(v(i), A(i,5), A(i,6))
+          exit
+        end if
+
+        dx = -gauss(x, A(i,5), A(i,6))
+        x = s/dx + x
+      end do
+    end do
+
+
+    ! Ghost class TODO
+    x = A(filas, 5)
+    do j = 1, 100
+      ! límite superior de probabilidad de ocupar theta asientos (0.1)
+      s = 0.1 - 1 + probabilidad(x, A(filas,5), A(filas,6))
+
+      if (abs(s) < tol) then
+        v(filas) = x
+        !print *, "GHOST CLASS"
+        !print *, filas, v(filas), j
+
+        !print *, 0.1
+        !print *, 1 - probabilidad(v(filas), A(filas,5), A(filas,6))
+        exit
+      end if
+
+      dx = -gauss(x, A(filas,5), A(filas,6))
+      x = s/dx + x
+    end do
+  end subroutine
+
+
+  subroutine vagones(A, filas, v, array, n, prob)
+    implicit none
+    integer, intent(in) :: n, filas
+    ! TODO inout
+    real(8), intent(inout) :: A(filas, 6), v(filas)
+    real(8), intent(inout) :: array(n, 2), prob(n,2)
+
+    integer :: nplazas
+    integer :: tasavagon
+    real(8) :: tasapax
+
+    integer :: i, clase
+    real(8) :: s, temp
+
+
+    ! TODO
+    v(filas) = dble(321)
+
+
+    nplazas = 80
+    tasavagon = 0
+    tasapax = 0
+
+    clase = 1
+
+    s = 0.d0
+
+    do i = 1, n
+
+      if (i > v(clase)) clase = clase + 1
+
+      if (ceiling(i/dble(nplazas)) - ceiling((i-1)/dble(nplazas)) == 1) then
+        s = s - tasavagon
+      end if
+
+      temp = A(clase,1) - tasapax
+      !print*, temp
+      temp = temp*prob(i, 2)
+      !print*, temp
+      s = s + temp !A(clase, 1) - tasapax
+
+      array(i,1) = i
+      array(i, 2) = s
+
+      !print *, i, s, clase
+
+    end do
+
+
+
+  end subroutine
+
+
+
+
+
+
+end module
---
+++ modulos/main.f90
@@ -0,0 +1,157 @@
+program main
+  use lib
+  implicit none
+
+  integer :: n, i, j, clase
+  real(8) :: t, mu, sigma, h, arrayA(320, 2), arrayB(320, 2), A(5,6), v(5), B(4,6), tarifas(6,2)
+
+  n = 320
+  mu = 264
+  sigma = 20
+
+  h = 1.5*3*2*sigma/dble(n)
+
+
+
+  ! do i = 1, n
+  !   t = i
+  !
+  !
+  !
+  !   arrayA(i+1, 1) = t
+  !   arrayA(i+1, 2) = 1 - probabilidad(t, mu, sigma)
+  !
+  !   !print *, i, arrayA(i+1, 1), arrayA(i+1, 2)
+  !
+  ! end do
+
+  ! datos de la tabla de valores EMSR b
+
+  A(1,:) = (/180, 14, 9, 0, 0, 0/)
+  A(2,:) = (/130, 87, 8, 0, 0, 0/)
+  A(3,:) = (/100, 89, 9, 0, 0, 0/)
+  A(4,:) = (/80, 14, 9, 0, 0, 0/)
+  A(5,:) = (/40, 60, 9, 0, 0, 0/)
+
+
+  ! prueba de congruencia
+
+  ! referencia:
+  ! https://youtu.be/mZY4CU05PLw
+
+  B(1,:) = (/500, 16, 5, 0, 0, 0/)
+  B(2,:) = (/420, 44, 15, 0, 0, 0/)
+  B(3,:) = (/290, 35, 11, 0, 0, 0/)
+  B(4,:) = (/125, 0, 0, 0, 0, 0/)
+
+
+
+  call valores_emsr(A, 5)
+
+  call proteger(A,5,v)
+
+
+  !print *, "MATRIZ VALORES EMSR: "
+
+  call show_array(A, 5, 6)
+
+  !print *, "NIVELES DE PROTECCION"
+
+  call show_array(v,5, 1)
+
+
+  v(5) = dble(321)
+  clase = 1
+
+  do i = 1, n
+    if (i > v(clase)) clase = clase + 1
+    t = i
+
+    mu = A(clase, 5)
+    sigma = A(clase, 3)
+
+    arrayA(i, 1) = t
+    arrayA(i, 2) = 1 - probabilidad(t, mu, sigma)
+
+    !print *, i, arrayA(i+1, 1), arrayA(i+1, 2)
+
+  end do
+
+
+
+  ! arrayA(1,2) = 0.92
+
+  call vagones(A, 5, v, arrayB, 320, arrayA)
+
+  !
+  !
+  !
+  ! do i = 1, 5
+  !   tarifas(i,1) = A(i,1)
+  !   tarifas(i,2) = A(i,4)
+  ! end do
+  !
+  ! tarifas(6,1) = 9.990
+  !
+  !
+  !
+  ! do j = 1, 5
+  !
+  ! sigma = A(j, 6)
+  ! mu = A(j, 5)
+  !
+  ! h = 3*2*sigma/dble(n)
+  !
+  ! do i = 0, n
+  !   t = -3*sigma + i*h + mu
+  !
+  !
+  !
+  !   arrayA(i+1, 1) = t
+  !   arrayA(i+1, 2) = -tarifas(j+1,1)/tarifas(j,2) + 1 - probabilidad(t, mu, sigma) !gauss(mu,sigma,t)!probabilidad(t, mu, sigma)
+  !
+  !   ! !print *, i, arrayA(i+1, 1), arrayA(i+1, 2)
+  !
+  ! end do
+  !
+  ! open(j, file = "data"//trim(str(j+10))//".dat", status = 'new')
+  ! do i=1,n+1
+  !    write(j,*) arrayA(i,1), arrayA(i,2)
+  ! end do
+  !
+  ! close(j)
+  !
+  !
+  !
+  ! end do
+
+
+
+  ! !print *, probabilidad(dble(0), dble(1), dble(2))
+
+  ! call random_number(h)
+  ! !print *, h
+  !
+  ! call random_number(h)
+  ! !print *, h
+
+  ! output data into a file
+
+  ! do i = 1, n
+  !   arrayB(i, 2) = arrayB(i,2)**(1/arrayA(i,2))!arrayB(i,2)*(arrayA(i,2)**(0.2))
+  ! end do
+
+
+
+
+
+
+   open(1, file = 'delta4.dat', status = 'new')
+   do i=1,320
+      write(1,*) arrayB(i,1), arrayB(i,2)
+   end do
+
+   close(1)
+
+
+end program
